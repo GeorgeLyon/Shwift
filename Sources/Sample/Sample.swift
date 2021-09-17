fb36dc503b320f6c6347ffe8709ba4c965cd4153
George <GeorgeLyon@users.noreply.github.com>
2021-09-17 13:17:29 -0700

    Add mapping functionality

--- Sources/Sample/Sample.swift
+++ Sources/Sample/Sample.swift
@@ -8,6 +8,9 @@ struct Sample: Script {
     let cat = try executable(named: "cat")
     let echo = try executable(named: "echo")
     let sed = try executable(named: "sed")
-    try await echo("Foo") | sed(#"s/\(.*\)/\1 \1/"#) | cat()
+    
+    print(try await echo("Foo") | sed(#"s/\(.*\)/\1\n\1/"#) | cat() | collect())
+    
+    print(try await echo("Foo") | map { "\($0)\n\($0)" } | collect())
   }
 }
---
+++ Sources/Script/Builtins.swift
@@ -0,0 +1,58 @@
+
+import Shell
+
+public extension Script {
+  
+  func map(transform: @escaping (String) async throws -> String) -> Shell._Invocation<Void> {
+    compactMap(transform: transform)
+  }
+  
+  func compactMap(transform: @escaping (String) async throws -> String?) -> Shell._Invocation<Void> {
+    Shell._Invocation {
+      try await Shell.current.builtin { handle in
+        for try await line in handle.input.lines {
+          if let line = try await transform(line) {
+            try await handle.output.withTextOutputStream { stream in
+              print(line, to: &stream)
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  func collect() -> Shell._Invocation<[String]> {
+    reduce(into: []) { $0.append($1) }
+  }
+  
+  func reduce<T>(
+    into initialResult: T,
+    _ updateAccumulatingResult: @escaping (inout T, String) async throws -> Void
+  ) -> Shell._Invocation<T> {
+    Shell._Invocation {
+      try await Shell.current.builtin { handle in
+        var result = initialResult
+        for try await line in handle.input.lines {
+          try await updateAccumulatingResult(&result, line)
+        }
+        return result
+      }
+    }
+  }
+  
+  func reduce<T>(
+    _ initialResult: T,
+    _ nextPartialResult: @escaping (T, String) async throws -> T
+  ) -> Shell._Invocation<T> {
+    Shell._Invocation {
+      try await Shell.current.builtin { handle in
+        var result = initialResult
+        for try await line in handle.input.lines {
+          result = try await nextPartialResult(result, line)
+        }
+        return result
+      }
+    }
+  }
+  
+}
