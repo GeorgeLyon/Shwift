5747b7c9ad0617548a08059bedbc88ea667beb9b
wti <wti@users.noreply.github.com>
2024-03-18 15:05:10 -0700

    docc comments for output and list comprehensions

--- Sources/Script/List Comprehensions.swift
+++ Sources/Script/List Comprehensions.swift
@@ -1,9 +1,15 @@
 import Shwift
 
 /**
- By default, shell output is processed as a list of lines
- */
+ Converts shell input stream to output stream via transform.
 
+ By default, input is segmented by newline, and a newline is printed at the end of each output segment.
+ - Parameters:
+ - delimiter: Character to split input into segments (defaults to newline)
+ - terminator: String printed at the end of each output  (defaults to newline)
+ - transform: converts String to String
+ - Returns: ``Shell/PipableCommand``
+ */
 public func map(
   segmentingInputAt delimiter: Character = "\n",
   withOutputTerminator terminator: String = "\n",
@@ -12,6 +18,16 @@ public func map(
   compactMap(segmentingInputAt: delimiter, withOutputTerminator: terminator, transform: transform)
 }
 
+/**
+ Converts shell input stream to output stream via transform, ignoring nil input
+
+ By default, input is segmented by newline, and a newline is printed at the end of each output segment.
+ - Parameters:
+ - delimiter: Character to split input into segments (defaults to newline)
+ - terminator: String printed at the end of each output  (defaults to newline)
+ - transform: converts String to String
+ - Returns: ``Shell/PipableCommand``
+ */
 public func compactMap(
   segmentingInputAt delimiter: Character = "\n",
   withOutputTerminator terminator: String = "\n",
@@ -30,6 +46,14 @@ public func compactMap(
   }
 }
 
+/**
+ Returns the result of processing input elements using the given closure and mutable initial value to accumulate the result.
+ - Parameters:
+   - initialResult: mutable initial value to accumulate the result
+   - delimiter: Character to split input into segments passed to closure (defaults to newline)
+   - updateAccumulatingResult: A closure over the mutable accumulator and the next String.
+ - Returns: ``Shell/PipableCommand``
+*/
 public func reduce<T>(
   into initialResult: T,
   segmentingInputAt delimiter: Character = "\n",
@@ -45,6 +69,14 @@ public func reduce<T>(
   }
 }
 
+/**
+ Returns the result of processing input elements using the given closure and mutable initial value to accumulate the result.
+ - Parameters:
+ - initialResult: initial value passed to initial closure
+ - delimiter: Character to split input into segments passed to closure (defaults to newline)
+ - nextPartialResult: A closure over the initial or previous value and the next String segment that returns the next value
+ - Returns: ``Shell/PipableCommand``
+ */
 public func reduce<T>(
   _ initialResult: T,
   segmentingInputAt delimiter: Character = "\n",
--- Sources/Script/Output Capture.swift
+++ Sources/Script/Output Capture.swift
@@ -1,9 +1,23 @@
+/**
+ Captures operation output as text.
+
+ By default, input is processed per line (delimited by newline),
+ and the result String contains each output line separated by newlines (but without an terminating newline).
+
+ - Parameters:
+ - delimiter: Character to split input into segments (defaults to newline)
+   - separator: String used to join output items into a single String (defaults to newlin)
+   - operation: closure writing to output stream to capture
+ - Throws: rethrows errors thrown by underlying operations
+ - Returns: String of output items delimited by separator
+ */
 public func outputOf(
   segmentingInputAt delimiter: Character = "\n",
   withOutputSeparator separator: String = "\n",
   _ operation: @escaping () async throws -> Void
 ) async throws -> String {
-  let lines = try await Shell.PipableCommand(operation) 
+  let lines =
+    try await Shell.PipableCommand(operation)
     | reduce(into: [], segmentingInputAt: delimiter) { $0.append($1) }
   return lines.joined(separator: separator)
 }
