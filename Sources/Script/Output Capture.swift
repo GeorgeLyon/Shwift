c44c0ab64e670500529966f59be40735f973cd88
George <GeorgeLyon@users.noreply.github.com>
2024-03-22 18:10:08 +0000

    Remove unused import

--- Sources/Script/Output Capture.swift
+++ Sources/Script/Output Capture.swift
@@ -1,5 +1,3 @@
-import Shwift
-
 public func outputOf(_ operation: @escaping () async throws -> Void) async throws -> String {
   let lines = try await Shell.PipableCommand(operation) | reduce(into: []) { $0.append($1) }
   return lines.joined(separator: "\n")
